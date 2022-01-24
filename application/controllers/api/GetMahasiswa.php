0b73074acf4fe070c37886f10561573cfd6d593c
Riekyazizah17 <86046208+Riekyazizah17@users.noreply.github.com>
2022-01-24 13:26:49 +0700

    Add files via upload

---
+++ application/controllers/api/GetMahasiswa.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH. "libraries/Format.php";
+require APPPATH. "libraries/RestController.php";
+use chriskacerguis\RestServer\RestController;
+
+class GetMahasiswa extends RestController
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('ModelMahasiswa');
+    }
+    public function index_get(){
+        $mhs=new ModelMahasiswa;
+        $resultmhs= $mhs->get_mahasiswa();
+        $this->response($resultmhs,200);
+    }
+}
+
+
+?>
---
+++ application/models/ModelMahasiswa.php
@@ -0,0 +1,11 @@
+<?php
+class ModelMahasiswa extends CI_Model
+{
+    public function get_mahasiswa(){
+        $query=$this->db->get('mahasiswa');
+        return $query->result();
+    }
+}
+
+
+?>
